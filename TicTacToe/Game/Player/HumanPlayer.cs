41d6103d454b219f5452293e0c272950b0161bd4
Mikael <mikaelmarkus1000@gmail.com>
2024-10-30 13:58:32 +0100

    Updaterade HumanPlayer

--- TicTacToe/Game/Player/HumanPlayer.cs
+++ TicTacToe/Game/Player/HumanPlayer.cs
@@ -12,4 +12,47 @@ namespace TicTacToe.Core.Game.Player
         private string symbol;
         private Color color;
         private int highscore;
+
+        public HumanPlayer(string name, string symbol, Color color)
+        {
+            this.name = name;
+            this.symbol = symbol;
+            this.color = color;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetSymbol()
+        {
+            return symbol;
+        }
+
+        public Color GetColor()
+        {
+            return color;
+        }
+
+        public int GetHighscore()
+        {
+            return highscore;
+        }
+
+        public void UpdateHighscore(int highscore)
+        {
+            SetHighscore(highscore);
+        }
+
+        // Private method to set highscore
+        private void SetHighscore(int highscore)
+        {
+            if (highscore > this.highscore)
+            {
+                this.highscore = highscore;
+            }
+        }
     }
+
+}
